eb0ac825823f6661f5482c5c37aaceb34f38da5a
liyu4 <yangxiaoxiu@live.cn>
2016-11-26 23:43:49 +0800

    color log

---
+++ log/log.go
@@ -0,0 +1,39 @@
+package log
+
+import (
+	"github.com/op/go-logging"
+	"os"
+)
+
+var log *logging.Logger
+
+func init() {
+
+	log = logging.MustGetLogger("example")
+	// Example format string. Everything except the message has a custom color
+	// which is dependent on the log level. Many fields have a custom output
+	// formatting too, eg. the time returns the hour down to the milli second.
+	var format = logging.MustStringFormatter(
+		`%{color}%{time:15:04:05.000} %{shortfunc} ▶ %{level:.4s} %{id:03x}%{color:reset} %{message}`,
+	)
+
+	// For demo purposes, create two backend for os.Stderr.
+	backend1 := logging.NewLogBackend(os.Stderr, "", 0)
+	backend2 := logging.NewLogBackend(os.Stderr, "", 0)
+
+	// For messages written to backend2 we want to add some additional
+	// information to the output, including the used log level and the name of
+	// the function.
+	backend2Formatter := logging.NewBackendFormatter(backend2, format)
+
+	// Only errors and more severe messages should be sent to backend1
+	backend1Leveled := logging.AddModuleLevel(backend1)
+	backend1Leveled.SetLevel(logging.ERROR, "")
+
+	// Set the backends to be used.
+	logging.SetBackend(backend1Leveled, backend2Formatter)
+}
+
+func NewLog() *logging.Logger {
+	return log
+}
