f8cbe9a7795ef614010fdaabb265a73253015e7c
liyu4 <yangxiaoxiu@live.cn>
2016-11-27 22:00:09 +0800

    print receive signal

--- main.go
+++ main.go
@@ -7,24 +7,28 @@ import (
 
 	"chill/command"
 	"chill/config"
+	colorlog "chill/log"
 	"chill/runner"
 )
 
 func main() {
+	var log = colorlog.NewLog()
 	conf := config.GetConfigs()
 
 	abspath, _ := filepath.Abs(conf.Directory)
 
 	patterns := conf.Patterns
-	cmd := command.New(conf.Command)
+	cmd := command.NewCommand(conf.Command)
 
 	r := runner.NewRunner(abspath, patterns, cmd)
 
 	go func() {
-		ch := make(chan os.Signal)
+		ch := make(chan os.Signal, 1)
 		signal.Notify(ch, os.Interrupt)
 
-		<-ch
+		/// Block until a signal is received.
+		s := <-ch
+		log.Infof("Got signal :%s", s.String())
 		r.Exit()
 	}()
 	r.Start()
